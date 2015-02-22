b0d6117790b29b8d74e47aa62450274b3079eda9
Anirut Worakitrungruang <ruttario@gmail.com>
2015-02-21 17:06:46 -0800

    Branch by Anirut Worakitrungruang

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions allow for calculating an inverse of an invertible matrix.
+## As the calculation of an inverse of a matrix is time-consuming, caching is 
+## leveraged in order to save processing time if calculated result is stored in
+## the cache.
 
-## Write a short comment describing this function
-
+## This function returns the a 2x2 matrix that contains 4 functions for
+## 1. storing given invertible matrix in cache (using assignment operator '<<-')
+## 2. getting invertible matrix from cache
+## 3. storing given inverse of an invertible matrix
+## 4. getting inverse of an invertible matrix from cache
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversex <- NULL
+        set <- function(y) {
+                x <<- y
+                inversex <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inversex <<- inverse
+        getinverse <- function() inversex
+        matrix(list(set = set,
+                    get = get,
+                    setinverse = setinverse,
+                    getinverse = getinverse),
+               nrow=2, ncol=2)
 }
 
-
-## Write a short comment describing this function
-
+## This function returns the inverse of given matrix x by either doing 
+## the calculation by itself or retrieving the cached calculated result from
+## function makeCacheMatrix().
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Retrieve cached inverse of x
+        inversex <- x[[4]]()
+        ## Check to see cached inverse of x is not null
+        if(!is.null(inversex)) {
+                ## Return cached inverse of x
+                message("getting cached inverse of x")
+                return(inversex)
+        }
+        data <- x[[2]]()
+        ## If given matrix x is NOT equal to cached matrix x,
+        ## cache x and calculated inverse of x
+        message("calculating inverse of x")
+        inversex <- solve(data)
+        x[[3]](inversex)
+        ## Return newly calculated inverse of x
+        inversex
 }
